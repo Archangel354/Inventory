e2494c334d93f42ea6e16af28415280ad1bdbccd
Archangel354 <rduncan354@yahoo.com>
2017-08-13 10:41:46 -0400

    updated MainActivity to get the big green button to go to the EditorActivity.

--- app/src/main/java/com/example/android/inventory/MainActivity.java
+++ app/src/main/java/com/example/android/inventory/MainActivity.java
@@ -2,30 +2,63 @@ package com.example.android.inventory;
 
 import android.app.LoaderManager;
 import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import com.example.android.inventory.data.InventoryContract.ProductEntry;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+import static butterknife.ButterKnife.findById;
+
+import static com.example.android.inventory.R.id.image;
 
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     // Identifier for the Iventory data loader
     private static final int INVENTORY_LOADER = 0;
 
-    /** Adapter for the ListView */
+    /**
+     * Adapter for the ListView
+     */
     InventoryCursorAdapter mCursorAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup big green + button to open EditorActivity
+        ImageButton btnAddProduct = (ImageButton) findViewById(R.id.btnAddProduct);
+        btnAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
 
         // Find the ListView which will be populated with the product data
         ListView inventoryListView = (ListView) findViewById(R.id.list);
@@ -63,8 +96,37 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         // Kick off the loader
         getLoaderManager().initLoader(INVENTORY_LOADER, null, this);
+    }
 
 
+
+
+
+    /**
+     * Helper method to insert hardcoded product data into the database. For debugging purposes only.
+     */
+    private void insertProductt() {
+        // Create a ContentValues object where column names are the keys,
+        // and hammer's attributes are the values.
+        ContentValues values = new ContentValues();
+        values.put(ProductEntry.COLUMN_PRODUCT_NAME, "hammer");
+        values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, "1");
+        values.put(ProductEntry.COLUMN_PRODUCT_PRICE, "5.00");
+        values.put(ProductEntry.COLUMN_PRODUCT_IMAGE, image);
+
+        // Insert a new row for Toto into the provider using the ContentResolver.
+        // Use the {@link ProductEntry#CONTENT_URI} to indicate that we want to insert
+        // into the pets database table.
+        // Receive the new content URI that will allow us to access Toto's data in the future.
+        Uri newUri = getContentResolver().insert(ProductEntry.CONTENT_URI, values);
+    }
+
+    /**
+     * Helper method to delete all pets in the database.
+     */
+    private void deleteAllProducts() {
+        int rowsDeleted = getContentResolver().delete(ProductEntry.CONTENT_URI, null, null);
+        Log.v("MainActivity", rowsDeleted + " rows deleted from products database");
     }
 
     @Override
@@ -79,6 +141,21 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
+    public void fetchImage(View view) throws IOException {
+
+        File folder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/demoImage.jpg/");
+        FileInputStream fis = new FileInputStream(folder);
+        byte[] image = new byte[fis.available()];
+        fis.read(image);
+        ContentValues values = new ContentValues();
+        values.put("image", image);
+        values.put(ProductEntry.COLUMN_PRODUCT_IMAGE, image);
+        //getContentResolver().insert(ProductEntry.CONTENT_URI, "image.png");
+        fis.close();
+        Toast.makeText(this, "Image Fetched", Toast.LENGTH_SHORT).show();
 
     }
 }
